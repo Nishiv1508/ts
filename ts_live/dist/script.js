e1ba6e474899e8341402b9870271eceb70117904
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-10 19:27:46 +0530

    classes and access specifier

--- ts_live/dist/script.js
+++ ts_live/dist/script.js
@@ -1,6 +1,20 @@
 "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
 // tsc --init
-console.log("Typescript is here");
-console.log("TS is amazing");
+class User {
+  email;
+  name;
+  #city = "Ahmedabad";
+  _age = 20;
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+    console.log(this.email, this.name);
+  }
+  get() {
+    console.log(this.#city, this._age);
+  }
+}
+const nish = new User("n@n.com", "Nishiv");
+nish.get();
 //# sourceMappingURL=script.js.map
--- ts_live/src/script.ts
+++ ts_live/src/script.ts
@@ -1,3 +1,22 @@
 // tsc --init
-console.log("Typescript is here");
-console.log("TS is amazing");
+class User {
+  email: string;
+  name: string;
+  readonly #city: string = "Ahmedabad";
+  _age: number = 20;
+
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+    console.log(this.email, this.name);
+  }
+
+  get() {
+    console.log(this.#city, this._age);
+  }
+}
+
+const nish = new User("n@n.com", "Nishiv");
+nish.get();
+
+export {};
