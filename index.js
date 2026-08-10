b956eb0350279e4901c22360d2a36af75052914d
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-10 18:39:15 +0530

    fix: tsc compiled

--- index.js
+++ index.js
@@ -72,4 +72,51 @@ let myUser = {
     isActive: false,
 };
 myUser.email = "m@gmail.com";
+//arrays
+const superHeroes = [];
+const heroPower = [];
+const allUSers = [];
+const mlModels = [[255, 255, 255], []];
+superHeroes.push("spidey");
+heroPower.push(2);
+allUSers.push({ name: "", isActive: true });
+//Union
+let score = 33; //anyone datatype
+score = 44;
+score = "55";
+let nish = { name: "Nishiv", id: 764 };
+nish = { username: "Nishiv", id: 123 };
+function getDbId(id) {
+    // id.toLowerCase() -> This will throw error as id can be both string or number so have to check it first
+    if (typeof id === "string") {
+        return id.toLowerCase(); //Now this will work
+    }
+    else {
+        return id + 2; // 100% value is number only
+    }
+}
+getDbId(3);
+getDbId("bkhwd");
+const data = [1, 2, 3]; // either all number array or all strings array
+const data1 = ["fweh", 23, "wd"]; //anything number or string
+let seatAllotment = "middle";
+// seatAllotment = "crew"   -> not possible
+//tuples
+let user4 = ["hc", 131, true]; //in a specific mentioned order. Order of the array matters
+let rgb = [255, 0, 255];
+let newUser = [112, "abc@gmail.com"];
+newUser[1] = "test@gmail.com";
+const hcSeat = "window" /* SeatChoice.WINDOW */; //only options available in the enum can be put
+const n = {
+    dbId: 214,
+    email: "b",
+    userId: 469132,
+    githubToken: "hdw",
+    startTrail: () => {
+        return "trail started";
+    },
+    getCoupon: (name, value) => {
+        return 10;
+    },
+};
 export {};
