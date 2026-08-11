e271602beafcf8684fbae600f81aec5c8d24d4e4
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-11 11:43:55 +0530

    feat: classes implement interface

--- ts_live/dist/script.js
+++ ts_live/dist/script.js
@@ -1,20 +1,71 @@
 "use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
 // tsc --init
+Object.defineProperty(exports, "__esModule", { value: true });
+class Instagram {
+    cameraMode;
+    filter;
+    burst;
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class YT {
+    cameraMode;
+    filter;
+    burst;
+    short;
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+}
 class User {
-  email;
-  name;
-  #city = "Ahmedabad";
-  _age = 20;
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    console.log(this.email, this.name);
-  }
-  get() {
-    console.log(this.#city, this._age);
-  }
+    email;
+    name;
+    // public email: string;
+    // name: string; //by default public
+    city = "Ahmedabad"; //we can use # also for private data member
+    _age = 20; // also can be written as protected
+    count = 1;
+    // constructor(email: string, name: string) {
+    //   this.email = email;
+    //   this.name = name;
+    //   console.log(this.email, this.name);
+    // }
+    //this is also correct if u dont want to declare members at the top of class
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.email = email;
+        this.name = name;
+        console.log(this.email, this.name);
+    }
+    get getData() {
+        return `city: ${this.city} and age: ${this._age}`;
+    }
+    get getCount() {
+        return this.count;
+    }
+    set setCount(count) {
+        this.count = count;
+    }
+    deleteToken() {
+        console.log("Deleted");
+    }
+}
+class SubUser extends User {
+    isFamily = true;
+    changeAge() {
+        this._age = this._age + 1;
+    }
 }
 const nish = new User("n@n.com", "Nishiv");
-nish.get();
+let data = nish.getData;
+nish.setCount = 2;
+console.log(nish.getCount);
+console.log(data);
 //# sourceMappingURL=script.js.map
--- ts_live/src/script.ts
+++ ts_live/src/script.ts
@@ -1,22 +1,79 @@
 // tsc --init
-class User {
-  email: string;
-  name: string;
-  readonly #city: string = "Ahmedabad";
-  _age: number = 20;
 
-  constructor(email: string, name: string) {
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burst: number;
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+  ) {}
+}
+
+class YT implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+    public short: string,
+  ) {}
+}
+
+class User {
+  // public email: string;
+  // name: string; //by default public
+  private readonly city: string = "Ahmedabad"; //we can use # also for private data member
+  _age: number = 20; // also can be written as protected
+  private count: number = 1;
+
+  // constructor(email: string, name: string) {
+  //   this.email = email;
+  //   this.name = name;
+  //   console.log(this.email, this.name);
+  // }
+
+  //this is also correct if u dont want to declare members at the top of class
+  constructor(
+    public email: string,
+    public name: string,
+  ) {
     this.email = email;
     this.name = name;
     console.log(this.email, this.name);
   }
 
-  get() {
-    console.log(this.#city, this._age);
+  get getData(): string {
+    return `city: ${this.city} and age: ${this._age}`;
+  }
+
+  get getCount(): number {
+    return this.count;
+  }
+
+  set setCount(count: number) {
+    this.count = count;
+  }
+
+  private deleteToken() {
+    console.log("Deleted");
+  }
+}
+
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeAge() {
+    this._age = this._age + 1;
   }
 }
 
 const nish = new User("n@n.com", "Nishiv");
-nish.get();
+let data = nish.getData;
+nish.setCount = 2;
+console.log(nish.getCount);
+console.log(data);
 
 export {};
